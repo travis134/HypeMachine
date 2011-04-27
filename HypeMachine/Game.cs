e6f8fdc597bce5d4eb9c863589f0407296ea3d41
Travis Sein <tsein@asu.edu>
2011-04-27 02:22:12 -0700

    first commit

---
+++ HypeMachine/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace HypeMachine
+{
+    public class Game
+    {
+        public String Name { get; set; }
+        public Uri Image { get; set; }
+        public String Price { get; set; }
+        public Uri Link { get; set; }
+        public String Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public String Platform { get; set; }
+
+        public Game() { }
+        public Game(String name, String link, String summary, String platform)
+        {
+            this.Name = name;
+            this.Link = new Uri(link);
+
+            Regex oRegex = new Regex("<img src=\"(?<IMAGE>https?://(?:[a-z-]+.)+[a-z]{2,6}(?:/[^/#?]+)+.(?:jpg|gif|png))\".*<b>Price:</b> \\$(?<PRICE>[0-9]*.[0-9]*) <b>ETA:</b> (?<RELEASEDATE>[0-9]+/[0-9]+/[0-9]+)<br>(?<DESCRIPTION>.*)");
+            MatchCollection oMatchCollection = oRegex.Matches(summary);
+            foreach (Match oMatch in oMatchCollection)
+            {
+                this.Image = new Uri(oMatch.Groups["IMAGE"].ToString());
+                this.Price = oMatch.Groups["PRICE"].ToString();
+                DateTime tempDate;
+                if (DateTime.TryParse(oMatch.Groups["RELEASEDATE"].ToString(), out tempDate))
+                {
+                    this.ReleaseDate = tempDate;
+                }
+                this.Description = oMatch.Groups["DESCRIPTION"].ToString();
+            }
+            this.Platform = platform;
+        }
+    }
+}
---
+++ HypeMachine/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Windows.Media.Imaging;
+
+namespace HypeMachine
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            String url = "http://www.gamestop.com/gs/content/feeds/gs_cs_All.xml";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.BeginGetResponse(ResponseHandler, request);
+        }
+
+        private void ResponseHandler(IAsyncResult asyncResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asyncResult);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                XmlReader reader = XmlReader.Create(response.GetResponseStream());
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+                System.Windows.Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    List<Game> games = new List<Game>();
+                    PanoramaItem panoramaItem = new PanoramaItem();
+                    ListBox listBox = new ListBox();
+                    foreach (SyndicationItem item in feed.Items)
+                    {
+                        games.Add(new Game(item.Title.Text.ToString(), item.Links[0].Uri.ToString(), item.Summary.Text.ToString(), item.Categories[0].Name.ToString()));
+                    }
+                    foreach (Game game in games)
+                    {
+                        listBox.Items.Add(new Image(){Source = new BitmapImage(game.Image)});
+                        System.Diagnostics.Debug.WriteLine(game.Image.ToString());
+                    }
+                    panoramaItem.Content = listBox;
+                    mainView.Items.Add(panoramaItem);
+                });
+            }
+        }
+    }
+}
